6d18097c75e430869d7ffdc106f6e4e2792b553b
Shubham Minglani <shubham@linux.com>
2016-04-20 15:00:40 +0530

    pull image if not available, providing tag is optional

--- shippy/dpyexec.py
+++ shippy/dpyexec.py
@@ -4,13 +4,22 @@ from random import randint
 
 def dpy_stitch(args):
     sane_input = {}
+
+    # remove unnecessary parameters from the args dictionary
     for param, value in args.items():
         if param != 'mode' and param != args['mode'] and value != None:
             sane_input.update({param: value})
+
+    # add tag to the image name if not provided by the user
+    if len(sane_input['image'].split(':')) == 1:
+        sane_input.update({'image': '{}:latest'.format(sane_input['image'])})
+
     return sane_input
 
 
 def dpy_run(client, sane_input):
+    if not client.images(name=sane_input['image']):
+        client.pull(sane_input['image'])
     if 'name' not in sane_input.keys():
         sane_input.update({'name': 'shippy_{}'.format(randint(1, 999999999))})
     client.create_container(**sane_input)
